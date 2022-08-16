181a8a471e01fdebc5ade5d426e67be52fecdf5f
Roy Smith <roy.smith.esq@gmail.com>
2022-08-16 15:52:31 +0000

    null

--- lib/src/messages/MessageApi.dart
+++ lib/src/messages/MessageApi.dart
@@ -1,7 +1,9 @@
-import 'package:xmpp_stone/src/data/Jid.dart';
-
-import '../elements/stanzas/MessageStanza.dart';
+import '../../xmpp_stone.dart';
 
 abstract class MessageApi {
+  /// Send a simple message. See also [Chat.sendMessage]
+  /// @param [Jid] to as a Jid
+  /// @param text
+  /// @paam [MessageStanzaType] optional message type enum (chat, groupchat, etc)
   void sendMessage(Jid to, String text, MessageStanzaType? messageType);
 }
--- lib/src/presence/PresenceApi.dart
+++ lib/src/presence/PresenceApi.dart
@@ -1,6 +1,7 @@
 import 'package:xmpp_stone/src/data/Jid.dart';
 import 'package:xmpp_stone/src/elements/stanzas/PresenceStanza.dart';
 
+/// sendPresence, subscribe, unsubscribe, acceptSubscription, declineSubscription, askDirectPresence
 abstract class PresenceApi {
   void sendPresence(PresenceData presence);
 
